7dc98f8a21589864a77cc082b6265d74ec45f99f
Mohammadyusuf2024 <yusufniyazi420@gmail.com>
2024-09-18 13:26:01 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let id = document.getElementById('btn').style.zIndex="-1";
+console.log(id);
+let id1 = document.getElementById('btn').style.backgroundColor="red";
+console.log(id);
+let id2 = document.getElementById('btn').innerHTML="Not Searchable";
+console.log(id2);
+const Background = document.getElementById('btn');
+Background.style.cssText=`
+     background-color: orange;
+    
+`;
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+  <div class="head" style="width:100%;height:100%; background-color:aqua"></div>  
+</body>
+</html>
